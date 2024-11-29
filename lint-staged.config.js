f9d40415f3d0cedc3f58e8a92e93c74c26c2a27f
Kemal Idris <keidscode@gmail.com>
2024-11-29 19:42:08 +0800

    chore: CI workflow ts-13 (#15)
    * chore(root): add CI workflow ts-13
    
    * chore(root): add milestone check on PR lint ts-13
    
    * chore: remove jest since I cant figure how to config it 😆 ts-13
    
    * chore: update lint scripts ts-13

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 /** @type {import('lint-staged').Config} */
 const config = {
   "packages/shared/src/*.{ts,js,cjs}": ["npm run lint:shared"],
-  "apps/backend/{src,test}/**/*.{ts,js,cjs}": ["npm run lint:be"],
+  "apps/backend/src/**/*.{ts,js,cjs}": ["npm run lint:be"],
   "apps/frontend/{app,src}/*.{ts,tsx,js,cjs}": ["npm run lint:fe"],
 };
 
