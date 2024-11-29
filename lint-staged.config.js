dd43c9753b62e1339919c55ddf93b1e8595a8ade
KeidsID <keidscode@gmail.com>
2024-11-29 15:04:59 +0800

    chore(root): add CI workflow ts-13

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,10 @@
 /** @type {import('lint-staged').Config} */
 const config = {
   "packages/shared/src/*.{ts,js,cjs}": ["npm run lint:shared"],
-  "apps/backend/{src,test}/**/*.{ts,js,cjs}": ["npm run lint:be"],
+  "apps/backend/{src,test}/**/*.{ts,js,cjs}": [
+    "npm run lint:be",
+    "npm run test:be",
+  ],
   "apps/frontend/{app,src}/*.{ts,tsx,js,cjs}": ["npm run lint:fe"],
 };
 
